ce202964a4fc8bd3a071be584fdeb60bb2b3ab1b
Aditya Sharma <adihgr18@gmail.com>
2023-04-08 18:04:18 +0530

    time complexity added

--- getTimeComplexity.py
+++ getTimeComplexity.py
@@ -1,19 +1,21 @@
+import cv2
 import time
-import numpy as np
-def my_image_processing_algorithm(image):
-    # Replace with your image processing algorithm
-    return processed_image
-
-# Measure the execution time for different input sizes
-for image_size in [100, 200, 400, 800]:
-    # Generate a random image of the given size
-    image = np.random.rand(image_size, image_size, 3)
-
-    # Measure the execution time of the algorithm
-    start_time = time.time()
-    processed_image = my_image_processing_algorithm(image)
-    end_time = time.time()
-    execution_time = end_time - start_time
-
-    print(f"Execution time for image of size {image_size}: {execution_time}")
-#To determine the time complexity of the algorithm, we can plot the execution time as a function of the input size, and analyze the slope of the resulting curve. For example, if the execution time increases linearly with the input size, the time complexity is O(n), where n is the input size. If the execution time increases quadratically with the input size, the time complexity is O(n^2), and so on.
+from he import he
+from clahe import clahe
+from DHE import dhe
+import matplotlib.pyplot as plt
+def getTimeComplexity(img,algo):
+    times = []
+    # Vary the size of the image and calculate the execution time for each size
+    for i in range(100, 2000, 100):
+        resized_img = cv2.resize(img, (i, i))
+        start_time = time.time()
+        equalized_img = algo(resized_img)
+        end_time = time.time()
+        times.append(end_time - start_time)
+    
+    # Plot the graph
+    plt.plot(range(100, 2000, 100), times)
+    plt.xlabel('Image Size')
+    plt.ylabel('Execution Time (seconds)')
+    plt.show()
--- main.py
+++ main.py
@@ -3,39 +3,46 @@ import cv2
 import matplotlib.pyplot as plt
 from getMSE import getMSE
 from getSNR import getSNR
-from wienerAndMedianFilter import wienerAndMedianFilter
+from getPSNR import getPSNR
+from getTimeComplexity import getTimeComplexity
+#from wienerAndMedianFilter import wienerAndMedianFilter
 from bilateralAndMedianFilter import bilateralAndMedianFilter
 from he import he
 from clahe import clahe
 from DHE import dhe
-def getEfficiencyParameters(imgA,imgB):
-    mse = getMSE()
 
-def denoise(img):
-    choice1 = input("Enter 1 for Wiener filter or 2 for bilateral filter: ")
-    if choice1 == 1:
-        img = wienerAndMedianFilter(img)
-    else: 
-        img = bilateralAndMedianFilter(img)
-    return img
+def getEfficiencyParameters(imgA,imgB):
+    print(f'SNR: {getSNR(imgB)}')
+    print(f'MSE: {getMSE(imgA,imgB)}')
+    print(f'PSNR: {getPSNR(imgA,imgB)}')
+
 if __name__ == "__main__":
     # read image from dataset
-    img = cv2.imread('dataset/1.png')
+    img = cv2.imread('dataset/3.png')
     print(f'SNR of input image is: {getSNR(img)}' )
     plt.imshow(img)
     choice1 = input("Enter 'yes' to apply denoising filter: ")
     if choice1 == 'yes':
-        img = denoise(img)
-    figure_size = 10
+        img = bilateralAndMedianFilter(img)
+    figure_size = 20
     plt.figure(figsize=(figure_size,figure_size))
     heResult =  he(img)
-    print(getSNR(heResult))
+    print("For HE")
+    imgA = cv2.imread('dataset/1.png')
+    getEfficiencyParameters( imgA, heResult)
+    #getTimeComplexity(imgA, dhe)
     plt.subplot(1, 3, 1),plt.imshow(heResult)
     plt.title('HE Image'), plt.xticks([]), plt.yticks([])
     claheResult = clahe(img)
+    
+    print("For CLAHE")
+    getEfficiencyParameters( imgA, claheResult)
     plt.subplot(1, 3, 2),plt.imshow(claheResult)
     plt.title('CLAHE Image'), plt.xticks([]), plt.yticks([])
+    """
     dheResult = dhe(img)
+    print("For DHE")
+    getEfficiencyParameters( imgA, dheResult)
     plt.subplot(1, 3, 3),plt.imshow(dheResult)
     plt.title('DHE Image'), plt.xticks([]), plt.yticks([])
-
+    """
